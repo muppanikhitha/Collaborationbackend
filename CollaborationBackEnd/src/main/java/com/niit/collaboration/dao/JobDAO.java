15481417c30b2c1579f175d7a8c6153ea8570418
muppanikhitha <muppanikhitha@gmail.com>
2016-11-02 01:40:43 +0530

    2

--- CollaborationBackEnd/src/main/java/com/niit/collaboration/dao/JobDAO.java
+++ CollaborationBackEnd/src/main/java/com/niit/collaboration/dao/JobDAO.java
@@ -1,5 +1,23 @@
 package com.niit.collaboration.dao;
 
-public interface JobDAO {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import com.niit.collaboration.model.Job;
+
+@Repository	
+
+public interface JobDAO {
+public boolean save(Job job);
+	
+	public boolean update(Job job);
+	
+	public boolean saveOrUpdate(Job job);
+	
+	public boolean delete(Job job);
+	
+	public Job get(String id);
+	
+	public List<Job> list();
 }
